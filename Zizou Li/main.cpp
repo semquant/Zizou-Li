281b5b92c0a9822c574e840d36af0541fb1a8bca
Zizou <Zizou@zizoudemacbook-air.local>
2016-11-23 13:50:11 -0800

    Initial Commit

---
+++ Zizou Li/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Zizou Li
+//
+//  Created by Zizou on 2016/11/23.
+//  Copyright © 2016年 Zizou. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
